1602304f801fd24158db2df2699d1386ef8da21b
taijun1007 <taijun1007@139.com>
2017-04-18 00:21:28 +0800

    baidumap

--- app/src/main/java/com/cmlab/servicetest/MapActivity.java
+++ app/src/main/java/com/cmlab/servicetest/MapActivity.java
@@ -3,6 +3,8 @@ package com.cmlab.servicetest;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.baidu.mapapi.map.MapView;
 
@@ -14,12 +16,46 @@ public class MapActivity extends Activity {
     public static final String TAG = "MapActivity";
 
     MapView mMapView = null;
+    ImageView mMapTypeImageView = null;
+    ImageView mTrafficImageView = null;
+    boolean isShiliangmap;
+    boolean isTrafficNo;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_baidumap);
         mMapView = (MapView) findViewById(R.id.bmapView);
+        mMapTypeImageView = (ImageView) findViewById(R.id.satellite);
+        mTrafficImageView = (ImageView) findViewById(R.id.traffic);
+        mMapTypeImageView.setImageResource(R.drawable.shiliangmap);
+        mTrafficImageView.setImageResource(R.drawable.trafficno);
+        isShiliangmap = true;
+        isTrafficNo = true;
+        mMapTypeImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isShiliangmap == true) {
+                    isShiliangmap = false;
+                    mMapTypeImageView.setImageResource(R.drawable.weixingmap);
+                } else {
+                    isShiliangmap = true;
+                    mMapTypeImageView.setImageResource(R.drawable.shiliangmap);
+                }
+            }
+        });
+        mTrafficImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isTrafficNo == true) {
+                    isTrafficNo = false;
+                    mTrafficImageView.setImageResource(R.drawable.trafficyes);
+                } else {
+                    isTrafficNo = true;
+                    mTrafficImageView.setImageResource(R.drawable.trafficno);
+                }
+            }
+        });
     }
 
     @Override

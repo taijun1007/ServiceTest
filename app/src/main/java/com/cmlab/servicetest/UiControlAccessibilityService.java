3426b621b29976833a48bb0aa3e55a9247625640
taijun1007 <taijun1007@139.com>
2017-04-17 21:29:30 +0800

    new ui control accessibility service

---
+++ app/src/main/java/com/cmlab/servicetest/UiControlAccessibilityService.java
@@ -0,0 +1,20 @@
+package com.cmlab.servicetest;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+/**
+ * Created by hunt on 2017/4/17.
+ */
+
+public class UiControlAccessibilityService extends AccessibilityService {
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
